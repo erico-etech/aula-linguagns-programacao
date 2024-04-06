e659c5652ffffaae3a5c1a18f4f40414d72eafff
erico-etech <162231952+erico-etech@users.noreply.github.com>
2024-04-05 20:38:32 -0400

    primeiro teste.cpp

---
+++ primeiro teste.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int idade;
+    float altura;
+
+    cin >> idade;
+    cin >> altura;
+
+    cout << "Altura: " << altura << endl;
+    cout << "idade: " << idade << endl;
+
+
+    return 0;
+}
